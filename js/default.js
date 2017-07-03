2dff0b676b55bb59ba636968ecaa04353697031c
ebonycitizen <coco_coco87@hotmail.com>
2017-07-03 17:17:34 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -5,16 +5,26 @@ var separate_time = function(time)
   var hours = time.getHours();
   var days = time.getDate();
   var month = time.getMonth();
-  var year = time.getYear();
+  var year = time.getFullYear();
   return [sec, min, hours, days, month, year];
 }
+var update = function()
+{
+  var now = new Date();
+  var counter = separate_time(now);
+  document.getElementById('countdown').textContent =
+    counter[5] + '年' +
+    counter[4] + '月' +
+    counter[3] + '日' +
+    counter[2] + '時' +
+    counter[1] + '分' +
+    counter[0] + '秒';
+  refresh();
+}
 
-var now = new Date();
-var counter = separate_time(now);
-document.getElementById('countdown').textContent =
-  counter[5] + '年' +
-  counter[4] + '月' +
-  counter[3] + '日' +
-  counter[2] + '時' +
-  counter[1] + '分' +
-  counter[0] + '秒';
+var refresh = function()
+{
+  setTimeout(update, 1000);
+}
+
+update();
